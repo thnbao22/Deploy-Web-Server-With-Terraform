298456d19d6c8dfa688356ddce9e45a8b4f93069
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-09 19:58:22 +0700

    Modify file main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -4,7 +4,13 @@ resource "aws_instance" "web-server" {
     # we use t2.micro
     instance_type = "t2.micro"
     key_name = "web-server"
-    user_data = filebase64("install_apache.sh")
+    user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install httpd
+                sudo systemctl start httpd
+                sudo systemctl status httpd
+                EOF
     availability_zone = "ap-southeast-1a"
     security_groups = [var.security_groups_web_server]
     subnet_id = var.public_subnet_id
