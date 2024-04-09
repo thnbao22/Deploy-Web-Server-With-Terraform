1819ce47abc77a453f5f3d3e438a0a5cdb40d27b
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-09 15:48:02 +0700

    Delete the var.instance_type and replace it by t2.micro

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web-server" {
     # Defome your AMI 
     ami = "ami-0fd77db8c27ba5cc5"
     # we use t2.micro
-    instance_type = var.instance_type
+    instance_type = "t2.micro"
     key_name = "web-server"
     user_data = filebase64("install_apache.sh")
     availability_zone = "ap-southeast-1a"
