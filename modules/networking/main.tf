0ff91dd3b89d07a69fc575a72e2f8449b199f987
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-09 16:25:56 +0700

    I add attribute map_public_ip_on_launch

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -11,6 +11,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.prod.id
   cidr_block = "10.10.1.0/24"
   availability_zone = "ap-southeast-1a"
+  map_public_ip_on_launch = true
   tags = {
     "Name" = "Public Subnet"
   }
