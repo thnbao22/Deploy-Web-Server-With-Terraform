69dfe36140e77f3000ae874e58665838b4cd43dc
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-09 15:49:57 +0700

    Delete the local variable instance_type

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,7 @@ provider "aws" {
 }
 
 locals {
-  instance_type = "t2.micro"
   location = "ap-southeast-1"
-
 }
 
 module "networking" {
@@ -13,7 +11,6 @@ module "networking" {
 }
 module "compute" {
   source = "../modules/compute"
-  instance_type = local.instance_type
   public_subnet_id = module.networking.public_subnet_id
   security_groups_web_server = module.networking.security_groups_web_server_id
 }
