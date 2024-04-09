5ff8dcb1af7326cd0fffe616ad493833db32b05b
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-09 16:11:43 +0700

    I fix the problem on the resource route table association because I use the wrong resoure on the route table id

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -38,7 +38,7 @@ resource "aws_route" "public_route" {
 
 resource "aws_route_table_association" "public" {
   subnet_id = aws_subnet.public_subnet.id
-  route_table_id = aws_route.public_route.id
+  route_table_id = aws_route_table.public.id
 }
 
 # Define local values for port
