78338a215ca4daded427b3e7bb3922d26c8e601f
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-09 19:59:12 +0700

    Modify file main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -42,52 +42,43 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-# Define local values for port
-locals {
-  # HTTP
-  port_in_80    = [
-    80
-  ]
-  # SSH
-  port_in_22    = [
-    22
-  ]
-  # HTTPS
-  port_in_443   = [  
-    443
-  ]
-}
 
 # SG for Web Server
-resource "aws_security_group" "web_server_sg" {
+resource "aws_security_group" "public_sg" {
  name = "Public Security Group"
  description = "Allow Web inbound traffic"
  vpc_id = aws_vpc.prod.id
- dynamic "ingress" {
-   for_each = toset(local.port_in_22)
-   content {
-     from_port = ingress.value
-     to_port = ingress.value
-     protocol = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
- }
- dynamic "ingress" {
-   for_each = toset(local.port_in_80)
-   content {
-     from_port = ingress.value
-     to_port = ingress.value
-     protocol = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
- }
- dynamic "ingress" {
-   for_each = toset(local.port_in_443)
-   content {
-     from_port = ingress.value
-     to_port = ingress.value
-     protocol = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
- }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+   "Name" = "Public SG"
+  }
 }
+
