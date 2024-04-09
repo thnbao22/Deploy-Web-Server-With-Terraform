4ba9801ba88f79a5c6ec7698c4fbc0409ccfc282
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-09 15:40:32 +0700

    Modify file main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
 provider "aws" {
-  region = "ap-southeast-1"
+  region = local.location
+}
+
+locals {
+  instance_type = "t2.micro"
+  location = "ap-southeast-1"
+
+}
+
+module "networking" {
+  source = "../modules/networking"
+}
+module "compute" {
+  source = "../modules/compute"
+  instance_type = local.instance_type
+  public_subnet_id = module.networking.public_subnet_id
+  security_groups_web_server = module.networking.security_groups_web_server_id
 }
